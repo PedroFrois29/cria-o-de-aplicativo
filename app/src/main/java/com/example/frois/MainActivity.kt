3cc9b8b4de59a3f3625a95f5fecf4b66f880b4f5
PedroFrois29 <pedroka.frois@gmail.com>
2023-04-11 17:17:26 -0300

    primeiro comit

---
+++ app/src/main/java/com/example/frois/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.frois
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.frois.ui.theme.FroisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FroisTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                   Cabecalho("Pedro Augusto Frois", "Aluno")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Cabecalho(nomeCompleto: String, cargo: String,modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+        Text(
+            text = nomeCompleto,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = cargo,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FroisTheme {
+       Cabecalho("Pedro Augusto Frois","Aluno")
+    }
+}
